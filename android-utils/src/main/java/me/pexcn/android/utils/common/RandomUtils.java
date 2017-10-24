2015fc521d256014d1808c4e6dca69860bbb1e4d
Max Witten <pexcn97@gmail.com>
2017-10-24 15:54:17 +0800

    Add RandomUtils

---
+++ android-utils/src/main/java/me/pexcn/android/utils/common/RandomUtils.java
@@ -0,0 +1,29 @@
+package me.pexcn.android.utils.common;
+
+import android.os.Build;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Created by pexcn on 2017-10-24.
+ */
+public class RandomUtils {
+    private RandomUtils() {
+    }
+
+    /**
+     * 获得一定范围内的随机数
+     *
+     * @param min 随机数最小值
+     * @param max 随机数最大值
+     * @return int 类型的随机数
+     */
+    public static int nextInt(int min, int max) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return ThreadLocalRandom.current().nextInt(min, max + 1);
+        } else {
+            return new Random().nextInt((max - min) + 1) + min;
+        }
+    }
+}
